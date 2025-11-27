37bc0182ec3c37f66ee9a5e860999ce5cca8365c
Marcus <marcus.vinicius.bittencourt.c@gmail.com>
2025-11-27 19:56:32 -0300

    requests de 10 min

--- src/utils/shouldMakeApiRequest.ts
+++ src/utils/shouldMakeApiRequest.ts
@@ -3,7 +3,7 @@ export default function shouldMakeApiRequest(): { shouldRequest: boolean; cacheT
   const dayOfWeek = now.getDay(); // 0=domingo, 1=segunda, 2=terça, 3=quarta, 4=quinta, 5=sexta, 6=sábado
 
   if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-    return { shouldRequest: true, cacheTime: 10 }; // 10 min
+    return { shouldRequest: true, cacheTime: 600 }; // 10 min
   }
-  return { shouldRequest: true, cacheTime: 10 }; // 20 min
+  return { shouldRequest: true, cacheTime: 1200 }; // 20 min
 }
